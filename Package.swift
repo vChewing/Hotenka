f0cb8c247512a0d6f52f1d4a5a7471b172539391
ShikiSuen <shikisuen@outlook.com>
2022-07-03 21:33:56 +0800

    First usable release.

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+  name: "Hotenka",
+  products: [
+    .library(
+      name: "Hotenka",
+      targets: ["Hotenka"]
+    )
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "Hotenka",
+      dependencies: []
+    ),
+    .testTarget(
+      name: "HotenkaTests",
+      dependencies: ["Hotenka"]
+    ),
+  ]
+)
